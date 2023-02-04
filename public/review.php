ab454cca17d2c73dfc30561d59f2a0170cafafdd
jplaluminium <jplaluminium@gmail.com>
2023-02-04 23:50:29 +0700

    first commit

---
+++ public/review.php
@@ -0,0 +1,211 @@
+﻿<html lang="en"><head>
+<meta charset="UTF-8">
+<!-- Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="เจพีแอล อลูมิเนียม แอนด์ กลาส, รับติดตั้ง กระจกอลูมิเนียม, รับออกแบบ กระจกอลูมิเนียม, รับติดตั้งกระจก ราคาถูก, รับติดตั้งกระจก, รับติดตั้งตกแต่งภายใน, รับกั้นห้องกระจก, รับกั้นห้องอาบน้ำ, รับติดตั้งประตูกระจก, รับติดตั้งหน้าต่างกระจก">
+<meta name="description" content="บริการติดตั้งกระจกอลูมิเนียมราคาถูก หน้าต่างประตูกระจก มุ้งลวด มุ้งจีบ กั้นห้อง กั้นห้องอาบน้ำ กระจกเทมเปอร์ ฝ้าทีบาร์ ฝ้าฉาบเรียบ ประตูเหล็ก งานกระจกอลูมิเนียมทุกชนิด ทุกแบบโดยช่างผู้ชำนาญและทีมงานมืออาชีพประสาบการ์มากกว่า 30 ปี ปรึกษาและประเมินราคาฟรี !!!">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index,follow">
+<meta name=rating content=General>
+<meta name="googlebot" content="archive" />
+<meta name="distribution" content="Global">
+<meta name="language" content="en-th">
+<meta name="rating" content="General">
+<meta name="expires" content="Never" />
+<meta content="7 days" name=revisit-after />
+<meta http-equiv="pragma" content="cache" />
+<meta property="og:image" content="images/slide1.jpg" />
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css?v=101">
+<link rel="stylesheet" type="text/css" href="css/style.css?v=101">
+<link rel="stylesheet" type="text/css" href="css/font.css?v=101">
+<link rel="stylesheet" type="text/css" href="css/product.css?v=101">
+
+<link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+<script type="text/javascript" src="js/jquery-1.11.3.min.js?v=101"></script>
+<title>ติดต่อสอบถาม ปรึกษา ประเมินราคา ดูหน้างานไม่มีค่าใช้จ่าย !!! กรุงเทพฯและปริมณฑล  -เจพีแอล อลูมิเนียม แอนด์ กลาส</title>
+
+</head>
+
+<body>
+<!-- Header -->
+<div class="navbar-wrapper">
+  <div class="bgblue">
+    <div class="container">
+      <div class="row m0">
+        <div class="col-xs-12 col-sm-8 p5 white font14">TEL: 081-483-7864,099-440-0555 | Line ID: @jplaluminium</div>
+        <div class="col-xs-12 col-sm-4 text-right hidden-xs">
+          <a href="https://www.facebook.com/jplaluminium/" target="_blank"><img src="images/faceicon1.png" width="30"></a>
+          <a href="https://line.me/R/ti/p/%40jplaluminium" target="_blank"><img src="images/lineicon1.png" width="30"></a>
+          <img src="images/google.png" width="30">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-default">
+      <div class="container" style="padding:0 15px;">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand visible-xs" href="./"><img src="images/logo.png" width="100px"></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li><a href="./" class="visible-xs">หน้าแรก</a></li>
+              <li><a href="gallery.php">ผลงาน</a></li>
+              <li><a href="services.php">บริการ</a></li>
+              <li><a href="./" class="hidden-xs zoom" style="border:none;padding: 5px 10px;margin: 0 10px;"><img src="images/logo_white.png" width="100"></a></li>
+              <li><a href="review.php">รีวิวลูกค้า</a></li>
+              <li><a href="contact.php">ติดต่อเรา</a></li>
+            </ul>
+        </div><!-- /.container -->
+      </div>
+  </nav>
+</div>
+<!-- Body -->
+<div class="bgscroll service" style="background-image: url(images/slide2.jpg);">
+  <div class="textslide"><br><h1>รีวิวจากลูกค้า</h1></div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<a href="images/jpl/review/review1.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review1.jpg);"></div></div>
+          	</div>
+        </a>
+        <a href="images/jpl/review/review2.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review2.jpg);"></div></div>
+          	</div>
+        </a>
+        <a href="images/jpl/review/review3.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review3.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review4.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review4.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review5.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review5.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review6.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review6.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review7.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review7.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review8.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review8.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review9.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review9.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review10.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review10.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review11.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review11.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review12.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review12.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review13.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review13.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review14.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review14.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review15.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review15.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review16.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review16.jpg);"></div></div>
+          	</div>
+        </a>
+		 <a href="images/jpl/review/review17.jpg" class='fresco' data-fresco-group='small'>
+          	<div class="col-xs-6 col-sm-4 col-md-3 p5">
+              	<div class="blockpro"><div class="picpro" style="background-image: url(images/jpl/review/review17.jpg);"></div></div>
+          	</div>
+        </a>
+	</div>
+</div>
+
+<!-- Footer -->
+<div class="bggray1">
+<div class="container">
+	<div class="row font16 white">
+		<div class="col-sm-5 col-md-3 col-md-offset-1 text-center">
+			<a href="./"><img src="images/logo_white.png" width="200" class="maxwidth"></a><br>
+		</div>
+		<div class="col-sm-4 col-md-4">
+			<h3 class="white">ติดต่อเรา</h3>
+		<img src="images/telephone1.png" width="17">  Tel : 081-483-7864,099-440-0555<br>
+		<img src="images/emailicon.png" width="17"> Email : jplaluminium@gmail.com<br>
+	   <a href="https://www.facebook.com/jplaluminium/" target="_blank"><img src="images/faceicon1.png" width="17"> บริษัท เจพีแอล อลูมิเนียม แอนด์ กลาส จำกัด</a><br><br>
+			5/10 หมู่ 12 ตำบล บางม่วง อำเภอ บางใหญ่ จังหวัดนนทบุรี 11140<br>
+		</div>
+		<div class="col-sm-3 col-md-4">
+            <h3>&nbsp;</h3>
+            <p><img src="images/lineicon1.png" width="18"> Line ID: @jplaluminium</p>
+            <img src="images/qrcode.jpg" width="150">
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="bgblue text-center">
+     <div class="container font14">
+        <div class="row" style="margin:5px;">
+            <span class="white">Copyright <i class="glyphicon glyphi-copyright-mark font12"></i> 2018 All Rights Reserved. Designed By <a href="https://www.facebook.com/fernaldesw" target="_blank" class="white">Korakrit</a></span>
+        </div>
+    </div>
+</div>
+
+
+<script src="js/bootstrap.min.js?v=1001"></script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) 
+    {
+        $(".bgfixed").css("background-attachment","scroll")
+    }
+});
+</script>
+
+</body>
+</html>
